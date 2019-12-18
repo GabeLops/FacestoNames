dc312a5069e30e60872d99f807507d6f94b543e0
GabeLops <gabrieldlops@gmail.com>
2019-12-18 12:57:36 -0800

    add name to face with textfield

---
+++ Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Gabriel Lops on 12/18/19.
+//  Copyright © 2019 Gabriel Lops. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+    self.name = name
+    self.image = image
+    }
+
+}
