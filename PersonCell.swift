69a33ecd177d968bbbebbb551c5241825537fac8
GabeLops <gabrieldlops@gmail.com>
2019-12-17 18:09:27 -0800

    add UI for face and names

---
+++ PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Gabriel Lops on 12/17/19.
+//  Copyright © 2019 Gabriel Lops. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
